8276dafce322f48b2aac7c44d50d282924e732eb
Tapwatero <70451052+Tapwatero@users.noreply.github.com>
2020-09-27 00:29:54 +0100

    Major Update
    Functioning Height Predictor

---
+++ predict.js
@@ -0,0 +1,18 @@
+function predictor() {
+    var mother = document.getElementById('mHeight').value;
+    var father = document.getElementById('fHeight').value;
+    let combined = +mother + +father;
+    let divided = combined / 2;
+    console.log(mother);
+
+    if (mother > 0 && father > 0) {
+        if (document.getElementById('male').checked) {
+            let predicted = divided + 12.7;
+            document.getElementById('predicted').innerHTML = "Your predicted adult height is " + predicted + "cm!";
+        } else if (document.getElementById('female').checked) {
+            let predicted = divided - 12.7;
+            document.getElementById('predicted').innerHTML = "Your predicted adult height is " + predicted + "cm!";
+        }
+    } 
+
+}
